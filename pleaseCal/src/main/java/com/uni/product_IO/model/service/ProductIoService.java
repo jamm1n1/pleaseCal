a19cce93ee72002445c8cfa7ab22478b41cff86a
sjho482@naver.com <seojemin@DESKTOP-VG484A2>
2022-03-18 15:55:33 +0900

    입출고완성

---
+++ pleaseCal/src/main/java/com/uni/product_IO/model/service/ProductIoService.java
@@ -0,0 +1,35 @@
+package com.uni.product_IO.model.service;
+
+import java.sql.Connection;
+import java.util.ArrayList;
+
+import com.uni.member.model.dao.MemberDao;
+import com.uni.member.model.vo.Member;
+import com.uni.product_IO.model.dao.ProductIoDao;
+import com.uni.product_IO.model.vo.Product_IO;
+import static com.uni.common.JDBCTemplate.*;
+
+public class ProductIoService {
+
+	public ArrayList<Product_IO> selectList() {
+		Connection conn = getConnection();
+
+		ArrayList<Product_IO> list = new ProductIoDao().selectList(conn);
+
+		close(conn);
+		 System.out.println(list+"서비스");
+		return list;
+	}
+
+	public Product_IO piO(int pid, int pnum) {
+Connection conn = getConnection();
+		
+		Product_IO pio = new ProductIoDao().piO(conn,pid,pnum);
+		
+		close(conn);
+		
+		
+		return pio;
+	}
+
+}
